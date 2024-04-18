39b96e447279f530dc61638abbdb1ff95a855e2d
alberto <vespertino@localhost>
2024-04-18 17:14:50 +0200

    Commit, previo al push, de la modificación del segundo

--- TrabajoEntornos_Erick_Alberto_Ismael/src/principal/Main.java
+++ TrabajoEntornos_Erick_Alberto_Ismael/src/principal/Main.java
@@ -5,8 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		Potencia potencia1 = new Potencia();
 		potencia1.calcularPotencia(2, 4);
+		Suma suma1 = new Suma();
+		suma1.calcularSuma(2, 4);
 		
-
+		//Comentario: Paso 3 realizado
 	}
 
 }
