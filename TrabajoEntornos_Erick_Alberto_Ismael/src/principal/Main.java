cb26435db4915924a0647d396bfa2b10e8e58793
ismas <emailprogramador2@entornos.com>
2024-04-18 16:36:24 +0200

    Commit 1 Eclipse Proyecto Erick, Alberto e Ismael

---
+++ TrabajoEntornos_Erick_Alberto_Ismael/src/principal/Main.java
@@ -0,0 +1,12 @@
+package principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Potencia potencia1 = new Potencia();
+		potencia1.calcularPotencia(2, 4);
+		
+
+	}
+
+}
---
+++ TrabajoEntornos_Erick_Alberto_Ismael/src/principal/Potencia.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Potencia {
+	
+	public void calcularPotencia(int numero1, int numero2) {
+		
+		System.out.println(numero1 +" elevado a "+ numero2 +
+				" es " + Math.pow(numero1, numero2));
+	}
+
+}
